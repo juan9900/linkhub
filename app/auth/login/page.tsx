f57c7599b9ff9c62ac54d53db72e4f0a4684f930
Juan <juanluislauretta@gmail.com>
2025-09-17 21:53:46 -0400

    WIP: Register system progess - form with validations

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,11 @@
+import { LoginForm } from "@/components/auth/LoginForm";
+
+const LoginPage = () => {
+  return (
+    <div className="flex justify-center items-center pt-10">
+      <LoginForm />
+    </div>
+  );
+};
+
+export default LoginPage;
